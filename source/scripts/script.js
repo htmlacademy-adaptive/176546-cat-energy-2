bcce5e17d1dc2f31ad9380a1f5e28d3a1c50305c
leokonv <leonid_sdl@mail.ru>
2026-05-30 01:26:54 +0400

    feat: удаление датасетов

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -1,15 +1,21 @@
-const navMain = document.querySelector('.main-nav__list');
+const mainNavList = document.querySelector('.main-nav__list');
+const mainHeader = document.querySelector('.main-header');
+const mainHeaderContainer = document.querySelector('.main-header__container');
+const mainNav = document.querySelector('.main-nav');
 const navToggle = document.querySelector('.main-nav__toggle');
 
-navMain.classList.remove('main-nav-list--nojs');
+mainNavList.classList.remove('main-nav-list--nojs');
+mainHeader.classList.remove('main-header--nojs');
+mainHeaderContainer.classList.remove('container--nojs');
+mainNav.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener('click', () => {
-  if (navMain.classList.contains('main-nav-list--closed')) {
-    navMain.classList.remove('main-nav-list--closed');
+  if (mainNavList.classList.contains('main-nav-list--closed')) {
+    mainNavList.classList.remove('main-nav-list--closed');
 
     navToggle.classList.add('main-nav__toggle--close');
   } else {
-    navMain.classList.add('main-nav-list--closed');
+    mainNavList.classList.add('main-nav-list--closed');
     navToggle.classList.remove('main-nav__toggle--close');
   }
 });
