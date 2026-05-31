9c7706f652767106369a9d1d11c7168953fae635
leokonv <leonid_sdl@mail.ru>
2026-05-31 13:46:37 +0400

    feat: исправление карты

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -3,11 +3,13 @@ const mainHeader = document.querySelector('.main-header');
 const mainHeaderContainer = document.querySelector('.main-header__container');
 const mainNav = document.querySelector('.main-nav');
 const navToggle = document.querySelector('.main-nav__toggle');
+const address = document.querySelector('.address');
 
 mainNavList.classList.remove('main-nav-list--nojs');
 mainHeader.classList.remove('main-header--nojs');
 mainHeaderContainer.classList.remove('container--nojs');
 mainNav.classList.remove('main-nav--nojs');
+address.classList.remove('address--nojs');
 
 navToggle.addEventListener('click', () => {
   if (mainNavList.classList.contains('main-nav-list--closed')) {
